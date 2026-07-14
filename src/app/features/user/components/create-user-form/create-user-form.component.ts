6bbdd10700d01e3128b199266ae2a15bde2f2622
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-13 20:36:04 -0500

    feat: agrego proyecto inicial, con las carpetas y componentes principales

---
+++ src/app/features/user/components/create-user-form/create-user-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user-form',
+  templateUrl: './create-user-form.component.html',
+  styleUrl: './create-user-form.component.scss'
+})
+export class CreateUserFormComponent {
+
+}
---
+++ src/app/features/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateUserFormComponent } from './components/create-user-form/create-user-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateUserFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
