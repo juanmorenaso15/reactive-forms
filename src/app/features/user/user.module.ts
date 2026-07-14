3d374d395523dee2c17a70678ed3451ae4522734
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-13 20:40:36 -0500

    refactor: modifico y pongo las nimprotaciones necesarias

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CreateUserFormComponent } from './components/create-user-form/create-user-form.component';
 
-
-
 @NgModule({
-  declarations: [
-    CreateUserFormComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CreateUserFormComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [CreateUserFormComponent],
 })
-export class UserModule { }
+export class UserModule {}
